a186f8f4d16cca41c230cfc3071ba485dce4f238
pal <pierre-alexandre@student.uclouvain.be>
2016-03-21 14:05:00 +0100

    debut

---
+++ trajAndAngle.m
@@ -0,0 +1,9 @@
+function [ T,z ] = trajAndAngle( theta,alpha )
+%Take a certain sigma_theta and a sigma_alpha random variable to create to
+%vectors:
+%-T: with the forme (x,y) the relative positions
+%-z:the angle observed by B
+
+
+end
+
