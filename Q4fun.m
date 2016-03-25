39c13e9df333f647900fbda669d5ea5904a7c095
SimonBoigelot <simon.boigelot@student.uclouvain.be>
2016-03-25 16:09:33 +0100

    débuggage 1

--- Q4fun.m
+++ Q4fun.m
@@ -1,7 +1,7 @@
 
 %Sequential Monte Carlo applied on the problem of the project
 %based on the received script by the professor Pierre-Antoine Absil
-function[X_target,Xtilde_target]=Q4fun(observer,target,bearingMeasurements,r,theta,s,c)
+function[X_target,Xtilde_target]=Q4fun(observer,target,bearingMeasurements,mu_r,mu_theta,mu_s,mu_c)
 %%
 %the systeme
 %x_(k+1)=F (x_k)+Gamma * v_k
@@ -25,16 +25,23 @@ Sigma_s = sqrt(0.1); %sqrt(variance) of the speed target
 
 Sigma_c = sqrt(0.1); %sqrt(variance) of the course of the target
 
+%out_noise_pdf= @(w) 1/sqrt((2*pi)^d_z*abs(det(Sigma_theta)))...
+%    * exp(-.5*(w-mu_theta)'*inv(Sigma_theta)*(w-mu_theta)); %normal sigma theta
+
 out_noise_pdf= @(w) 1/sqrt((2*pi)^d_z*abs(det(Sigma_theta)))...
-    * exp(-.5*(w-theta)'*inv(Sigma_theta)*(w-theta)); %normal sigma theta
+    * exp(-.5*w'*inv(Sigma_theta)*w); %normal sigma theta
 
 x_true = zeros(d_x,t_f +1);%we will work on that( the relative distance)
 z_true = bearingMeasurements; %according to the hypothesis, z is known
-
-x_true(:,0 +1)=[c+Sigma_c*randn(2,1);s+Sigma_s*randn(2,1)]-...
-    [observer(:,1);zeros(2,1)];%initialisation DOIT ETRE MODIFIE
+r = mu_r+Sigma_r*randn(1,1);
+theta = mu_theta+Sigma_theta*randn(1,1);
+s = mu_s + Sigma_s*randn(1,1);
+c = mu_c+Sigma_c*randn(1,1);
+x_true(:,1) = [r*sin(theta); -r*cos(theta); s*cos(c); s*sin(c)];
+%x_true(:,0 +1)=[mu_c+Sigma_c*randn(2,1);mu_s+Sigma_s*randn(2,1)]-...
+%    [observer(:,1);zeros(2,1)];%initialisation DOIT ETRE MODIFIE
 for t=0:t_f-1
-    epsilon_true=Gamma*eye(2)*(mu_v+ Sigma_v.*randn(d_v,1)); %process noise
+    epsilon_true=Gamma*(mu_v+ Sigma_v.*randn(d_v,1)); %process noise
     x_true(:,t+1 +1)=F(x_true(:,t +1))+ epsilon_true;
 end
 
@@ -48,8 +55,18 @@ X=cell(n,t_f +1); %right relative X postions
 Xtilde=cell(n,t_f +1); %predicitons for relative X positions 
 t=0;
 for i=1:n
-    X{i,t +1} =[c+Sigma_c*randn(2,1);s+Sigma_s*randn(2,1)]-...
-        [(r+Sigma_r*randn(2,1));zeros(2,1)];
+    if (theta >= 0 && theta < pi/2) 
+   % X{i,t +1} =[mu_c+Sigma_c*randn(2,1);mu_s+Sigma_s*randn(2,1)]-...
+        %[(mu_r+Sigma_r*randn(2,1));zeros(2,1)];
+        X{i,t +1} = [r*sin(theta); -r*cos(theta); s*cos(c); s*sin(c)];
+   % X{i,t +1} = [-observer(1,1)+2;-observer(2,1)+0.25;s*cos(c);s*sin(c)];
+    elseif (theta >= pi/2 && theta < pi)
+         X{i,t +1} = [-r*sin(theta); -r*cos(theta); s*cos(c); s*sin(c)];
+    elseif (theta >= pi && theta < 1.5*pi)
+         X{i,t +1} = [r*sin(theta); r*cos(theta); s*cos(c); s*sin(c)];
+    else
+        X{i,t +1} = [r*sin(theta); r*cos(theta); s*cos(c); s*sin(c)];
+    end
 end
 %Beginning of the loop
 
@@ -57,7 +74,7 @@ for t=0:t_f-1
     %Prediction
     
     for i=1:n
-        epsilon=Gamma*eye(2)*(mu_v+ Sigma_v.*randn(d_v,1)); %epsilon_k
+        epsilon=Gamma*(mu_v+ Sigma_v.*randn(d_v,1)); %epsilon_k
         Xtilde{i,t+1 +1} = F(X{i,t +1})+ epsilon;
     end
     
@@ -74,13 +91,13 @@ for t=0:t_f-1
     
 end
 
-%%Création of the neeeded vector to plot
-Xtilde_target=cell(n,t_f);%from x^t_1 to x^t_n
+%%Création of the needed vector to plot
+Xtilde_target = cell(n,t_f);%from x^t_1 to x^t_n
 X_target=cell(n,t_f);
 for i=1:n
    for j=1:t_f
-       Xtilde_target{i,j}=Xtilde{i,j+1}+[observer(:,t_f);zeros(2,1)];
-       X_target{i,j}=X{i,j+1}+[observer(:,t_f);zeros(2,1)];
+       Xtilde_target{i,j}=Xtilde{i,j+1};%+[observer(:,j);zeros(2,1)];
+       X_target{i,j}=X{i,j+1};%+[observer(:,j);zeros(2,1)];
    end
 end
 
