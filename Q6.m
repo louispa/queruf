a19c6f11ce4d263cf5e55e27fd2ca0483c334035
pal <pierre-alexandre@student.uclouvain.be>
2016-05-12 20:45:17 +0200

    bonne Q6

--- Q6.m
+++ Q6.m
@@ -30,8 +30,10 @@ plot(bound);
 hold on;
 plot(estimated_relative_error);
 hold off;
-
+title('Evolution of the CRLMB of the RMS error and the RMS error in function of the time');
 legend('Cramer Rao Lower Bound','RMS postition error');
+xlabel('iterations');
+ylabel('error');
 
 
 
--- Q6fun.m
+++ Q6fun.m
@@ -16,7 +16,7 @@ Ppyy=mu_s^2*sigma_c_squared*sin(mu_c)^2+sigma_s_squared*cos(mu_c)^2;
 Ppxy=(sigma_s_squared-mu_s^2*sigma_theta_squared)*sin(mu_c)*cos(mu_c);
 
 P1=[ [Pxx Pxy; Pxy Pyy] zeros(2,2) ;
-    zeros(2,2) [Ppxx Ppxy; Ppxy Ppyy] ]
+    zeros(2,2) [Ppxx Ppxy; Ppxy Ppyy] ];
 
 T=1;
 f=[eye(2) T*eye(2);zeros(2) eye(2)];
@@ -24,8 +24,7 @@ f_t=inv(f');
 f_1=inv(f);
 %initialisation
 Jk=cell(t_f,1);
-Jk{1}=inv(P1)
-Jk{1}
+Jk{1}=inv(P1);
 %loop
 for i=2:26
     Jk{i}= f_t*Jk{i-1}*f_1+sigma_theta_squared^-1*dG(X(:,i))*dG(X(:,i))';
@@ -33,11 +32,13 @@ end
 
 bound=zeros(26,1);
 for i=1:26
-   bound(i)=sqrt(Jk{i}(1,1)^-1 +Jk{i}(2,2)^-1); 
+   P=inv(Jk{i});
+   bound(i)=sqrt(P(1,1) +P(2,2)); 
 end
 
 end
 
+%G= constant +- artan( abs(x/y)
 function[y_out] = dG(x_in)
     if x_in(1)>=0 && x_in(2)>=0
        y_out = [1/(x_in(2)*(x_in(1)^2/x_in(2)^2 + 1));...
@@ -53,21 +54,3 @@ function[y_out] = dG(x_in)
            x_in(1)/(x_in(2)^2*(x_in(1)^2/x_in(2)^2 + 1)); 0 ; 0];
     end;
 end
-
-
-% function[y_out] = dG(x_in)
-%     if x_in(1)>=0 && x_in(2)>=0
-%        y_out = [1/(x_in(2)*(x_in(1)^2/x_in(2)^2 + 1));...
-%            -x_in(1)/(x_in(2)^2*(x_in(1)^2/x_in(2)^2 + 1)); 0 ; 0];
-%     elseif x_in(1)>0 && x_in(2)<0
-%        y_out = [-sign(x_in(1))/(abs(x_in(2))*(abs(x_in(1))^2/abs(x_in(2))^2 + 1));...
-%            (abs(x_in(1))*sign(x_in(2)))/(abs(x_in(2))^2*(abs(x_in(1))^2/abs(x_in(2))^2 + 1)); 0 ; 0];
-%     elseif x_in(1)<0 && x_in(2)<0
-%        y_out = [sign(x_in(1))/(abs(x_in(2))*(abs(x_in(1))^2/abs(x_in(2))^2 + 1));...
-%            -(abs(x_in(1))*sign(x_in(2)))/(abs(x_in(2))^2*(abs(x_in(1))^2/abs(x_in(2))^2 + 1)); 0 ; 0];
-%     else
-%        y_out = [-sign(x_in(1))/(abs(x_in(2))*(abs(x_in(1))^2/abs(x_in(2))^2 + 1));...
-%            (abs(x_in(1))*sign(x_in(2)))/(abs(x_in(2))^2*(abs(x_in(1))^2/abs(x_in(2))^2 + 1)); 0 ; 0];
-%     end;
-% end
-
